9c4e36c6c363270f16dc4711a3236226374f2f22
DavyKK <davykk04@gmail.com>
2022-06-25 01:15:22 +0300

    required changes in my index.htm and JS complete

--- javascript/script.js
+++ javascript/script.js
@@ -5,6 +5,7 @@ const mobileMenu = document.getElementById('mobile_menu');
 const closeButton = document.getElementById('close_button');
 const navMenu = document.querySelectorAll('.mobile_menu a');
 const feauteredSpeakers = document.getElementById('speakers_wrap');
+const mediaqueryList = window.matchMedia('(max-width: 768px)');
 
 hamburgerMenu.addEventListener('click', () => {
   mobileMenu.style.display = 'flex';
@@ -22,7 +23,7 @@ navMenu.forEach((element) => {
 
 // dynamicLoading
 
-const speakers = () => {
+const speakers= () => {
   let str = '';
   for (let i = 0; i < myObjects.length; i += 1) {
     str += `<div class="speaker_container">
